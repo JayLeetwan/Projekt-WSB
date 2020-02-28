ce330feac6ca56896f2fde47104d2de17f982251
JayLeetwan <56274500+JayLeetwan@users.noreply.github.com>
2020-02-28 20:51:39 +0100

    wszystko sie wypierdala

--- scripts/stronaLogowania/stronaLogowaniaFunkcje.js
+++ scripts/stronaLogowania/stronaLogowaniaFunkcje.js
@@ -1,8 +1,19 @@
+function checkCredentials(userName, password, usersTable) {
+    console.log(XXX);
+    for (i=0; usersTable.length; i++) {
+        console.log(userName)
+    }
+}
+
 function logingIn(form) {
+    console.log(XXX);
+    checkCredentials(form.login.value, form.haslo.value, users);
+    console.log(XXX);
     if (form.login.value == users.userName && form.haslo.value == users.password) {
     document.getElementById("stronalogowania").style.display = "none";
     } else {
     alert("Login or password is not correct!")
     }
     }
+
   
--- scripts/stronaLogowania/users.js
+++ scripts/stronaLogowania/users.js
@@ -1,20 +1,13 @@
-// var users = [
-//   { 
-//     fullName: "John Lenon",
-//     userName: "JohnyXXX",
-//     password: "XXX123"
-//   },
-//   {
-//     fullName: "Zbigniew S.",
-//     userName: "zbigniew Stonoga",
-//     password: "stonoga"
-//   }
-
-
-var users = { 
+var users = [
+  { 
     fullName: "John Lenon",
     userName: "JohnyXXX",
     password: "XXX123"
+  },
+  {
+    fullName: "Zbigniew S.",
+    userName: "zbigniew Stonoga",
+    password: "stonoga"
   }
-   
+]
   
