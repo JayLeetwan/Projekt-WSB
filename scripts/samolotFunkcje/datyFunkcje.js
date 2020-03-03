170a4f09e27e987878a0f815ffcaed8cc643a3e7
JayLeetwan <56274500+JayLeetwan@users.noreply.github.com>
2020-03-03 21:13:20 +0100

    zmiana w kalendarzach, proba ustawienia minimalnej daty dla drugiego pola

--- scripts/samolotFunkcje/datyFunkcje.js
+++ scripts/samolotFunkcje/datyFunkcje.js
@@ -1,23 +1,65 @@
-document.getElementById('dateFromContainer').valueAsDate = new Date();
-document.getElementById('dateToContainer').valueAsDate = new Date();
+        var chosenFlightDate = document.getElementById("dateFrom").value;
+
+        // document.getElementById("roundTripFlight").addEventListener("check", lotWDwieStrony);
+        function lotWDwieStrony() {
+          var checkBox = document.getElementById("roundTripFlight");
+          var text = document.getElementById("FinishDate");
+          if (checkBox.checked === true) {
+            text.style.display = "block";
+          } else {
+            text.style.display = "none";
+          }
+        }
+
+        // var todaysDate = new Date();
+        // var day = todaysDate.getDate();
+		    // var month = todaysDate.getMonth()+1;
+        // var year = todaysDate.getFullYear();
+        // if(day<10){
+        //     day='0'+day
+        // } 
+        // if(month<10){
+        //     month='0'+month
+        // }
+
+        $( function() {
+          $("#dateFrom").datepicker({
+          dateFormat: "dd-mm-yy",
+          changeMonth: true,
+          changeYear: true,
+          maxDate: '12M',
+          minDate: '0',
+            });
+        })
+
+      $( function() {
+          $("#dateTo").datepicker({
+          dateFormat: "dd-mm-yy",
+          changeMonth: true,
+          changeYear: true,
+          maxDate: '12M',
+          minDate: chosenFlightDate,
+            });
+        })
 
 
-function lotWDwieStrony() {
-    var checkBox = document.getElementById("roundTripFlight");
-    var text = document.getElementById("FinishDate");
-    if (checkBox.checked == true) {
-      text.style.display = "block";
-    } else {
-      text.style.display = "none";
-    }
-  }
+        
+// document.getElementById('dateFromContainer').valueAsDate = new Date();
+// document.getElementById('dateToContainer').valueAsDate = new Date();
+
+// function dateValidate() {
+//     var input = document.getElementsByClassName("dateContainer");
+//     var maxDate = new Date(todaysDate.getFullYear(), todaysDate.getMonth() +13, now.getDay());
+//     var minDate = new Date(todaysDate.getFullYear(), todaysDate.getMonth(), now.getDay());
+//     input.setAttribute("min", minDate);
+//     input.setAttribute("max", maxDate);
+//   }
+
+// $(function() {
+//     $("#dateFromContainer").dateFromContainer({
+//       minDate: 0,
+//       maxDate: 365
+//     });
   
-  function funkcjaDataWylotu() {
-    // var x = document.getElementById("myDate").value;
-    // document.getElementById("dataWylotu").innerHTML = x;
-  }
-  
-  function funkcjaDataPowrotu() {
-    // var x = document.getElementById("myDate").value;
-    // document.getElementById("dataPowrotu").innerHTML = x;
-  }
+//   });  
+
