bafa79fe42580526ef974ecb3e415c032218e489
JayLeetwan <56274500+JayLeetwan@users.noreply.github.com>
2020-03-06 18:21:32 +0100

    kalendarz działa!

--- scripts/samolotFunkcje/datyFunkcje.js
+++ scripts/samolotFunkcje/datyFunkcje.js
@@ -1,6 +1,3 @@
-        var chosenFlightDate = document.getElementById("dateFrom").value;
-
-        // document.getElementById("roundTripFlight").addEventListener("check", lotWDwieStrony);
         function lotWDwieStrony() {
           var checkBox = document.getElementById("roundTripFlight");
           var text = document.getElementById("FinishDate");
@@ -11,55 +8,23 @@
           }
         }
 
-        // var todaysDate = new Date();
-        // var day = todaysDate.getDate();
-		    // var month = todaysDate.getMonth()+1;
-        // var year = todaysDate.getFullYear();
-        // if(day<10){
-        //     day='0'+day
-        // } 
-        // if(month<10){
-        //     month='0'+month
-        // }
-
-        $( function() {
+        $(function () {
+          let chosenDate = document.getElementById("dateFrom");
           $("#dateFrom").datepicker({
-          dateFormat: "dd-mm-yy",
-          changeMonth: true,
-          changeYear: true,
-          maxDate: '12M',
-          minDate: '0',
-            });
-        })
-
-      $( function() {
+            dateFormat: "dd-mm-yy",
+            changeMonth: true,
+            changeYear: true,
+            maxDate: "12M",
+            minDate: new Date(),
+          });
           $("#dateTo").datepicker({
-          dateFormat: "dd-mm-yy",
-          changeMonth: true,
-          changeYear: true,
-          maxDate: '12M',
-          minDate: chosenFlightDate,
-            });
-        })
-
-
-        
-// document.getElementById('dateFromContainer').valueAsDate = new Date();
-// document.getElementById('dateToContainer').valueAsDate = new Date();
-
-// function dateValidate() {
-//     var input = document.getElementsByClassName("dateContainer");
-//     var maxDate = new Date(todaysDate.getFullYear(), todaysDate.getMonth() +13, now.getDay());
-//     var minDate = new Date(todaysDate.getFullYear(), todaysDate.getMonth(), now.getDay());
-//     input.setAttribute("min", minDate);
-//     input.setAttribute("max", maxDate);
-//   }
-
-// $(function() {
-//     $("#dateFromContainer").dateFromContainer({
-//       minDate: 0,
-//       maxDate: 365
-//     });
-  
-//   });  
-
+            dateFormat: "dd-mm-yy",
+            changeMonth: true,
+            changeYear: true,
+            maxDate: "12M",
+            minDate: new Date(),
+          });
+          $("#dateFrom").change(() => {
+            $("#dateTo").datepicker("option", "minDate", chosenDate ? chosenDate.value : new Date());
+          });
+        });
