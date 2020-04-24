7bc77f25a55899376d22c5fd90e3b7ba01de774b
JayLeetwan <56274500+JayLeetwan@users.noreply.github.com>
2020-04-24 19:40:30 +0200

    generowanie samolotu początek

--- scripts/rezerwacjaFunkcje/planePlanGenerate.js
+++ scripts/rezerwacjaFunkcje/planePlanGenerate.js
@@ -15,11 +15,25 @@ function wyborMiejsc() {
 
 
 
-function choseSeats() {
+function createAirplaneHtml() {
   var wayFrom = document.getElementById("startCity").value;
   var wayTo = document.getElementById("destinationCity").value;
   if (wayFrom === "Poznań" || wayFrom === "Warszawa" & wayTo === "Warszawa" || wayTo === "Poznań" ) {
-    document.getElementById('planePlan').src="bombardierPlan.html";
     alert("Index of startCity is: " + wayFrom + ", and index of destinationCity is: " + wayTo);
+    var seatsNumber = 60;
+    for (i=0; i < seatsNumber; i++) {
+      console.log('siedzenie numer'[i]);
+       return `<input id=` + i + ` type= 'checkbox'>`;
+    }
+  } else {
+      alert("duży samolot");
+      return `
+      <input id='1' type= 'checkbox'>
+      `
   }
 }
+
+function setHtmlIntoContainer(){
+  console.log('wykonuje setHtmlIntoContainer')
+  document.getElementById('samolot-plan-kontener').innerHTML = createAirplaneHtml(document.getElementById('linkDoWyboruMiejsc').click)
+} 
